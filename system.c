f5ccfb57b702f2af312262663fde5894a1d10fca
Thomas Peterson <trp46@uclive.ac.nz>
2021-04-30 19:27:41 +1200

    Add system modules

---
+++ system.c
@@ -0,0 +1,8 @@
+//*****************************************************************************
+//
+// system.c - helicopter system functionality
+//
+// Authors: T.R. Peterson M.G. Gardyne M. Comber
+// Date 19/04/2021
+//
+// Code Sourced from:  P.J. Bones  UCECE (acknowledged in function descriptions)
---
+++ system.h
@@ -0,0 +1,15 @@
+/*
+ * system.h
+ *
+ *  Created on: 30/04/2021
+ *      Author: trpet
+ */
+
+#ifndef SYSTEM_H_
+#define SYSTEM_H_
+
+
+
+
+
+#endif /* SYSTEM_H_ */
